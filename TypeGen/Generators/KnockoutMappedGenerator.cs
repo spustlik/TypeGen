1a37585db4273c39175eab56738b911d2d2a8584
Jan Stuchlik <jan.stuchlik@email.cz>
2020-08-21 15:24:09 +0200

    knockout refact, extra data

--- TypeGen/Generators/KnockoutMappedGenerator.cs
+++ TypeGen/Generators/KnockoutMappedGenerator.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace TypeGen.Generators
 {
+    /// <summary>
+    /// generates classes with usage of ko.observable and ko.observableArray 
+    /// using "map" and on-demand generation of referenced classes
+    /// Is is using typescript interfaces module as a input. Interfaces can be generated using ReflectionGenerator.
+    /// </summary>
     public class KnockoutMappedGenerator
     {
+        public static string KO_TYPE = "knockout_type";
+        public static string KO_ITEMTYPE = "knockout_itemtype";
         private List<InterfaceType> _toGenerate = new List<InterfaceType>();
         private HashSet<ClassType> _generated = new HashSet<ClassType>();
-        private Dictionary<InterfaceType, ClassType> _map = new Dictionary<InterfaceType, ClassType>();
+        private Dictionary<InterfaceType, DeclarationBase> _map = new Dictionary<InterfaceType, DeclarationBase>();
         private TypescriptModule _targetModule;
 
         public KnockoutMappedGenerator(TypescriptModule targetModule)
@@ -44,7 +51,8 @@ namespace TypeGen.Generators
             {
                 var intf = _toGenerate[0];
                 _toGenerate.RemoveAt(0);
-                var cls = GetClass(intf);
+                var target = GetInterfaceMap(intf);
+                var cls = (ClassType)target;
                 _generated.Add(cls);
                 GenerateClassContent(cls, intf);
             }
@@ -55,7 +63,7 @@ namespace TypeGen.Generators
             if (intf.IsExtending)
             {
                 var baseIntf = (InterfaceType)intf.ExtendsTypes.First().ReferencedType;
-                cls.Extends = GetClass(baseIntf);
+                cls.Extends = GetInterfaceMap(baseIntf);
                 if (intf.ExtendsTypes.Count() > 1)
                 {
                     //cls.Comment+="WARNING: interface is extending "+String.Join(",", intf.ExtendsTypes);
@@ -68,12 +76,28 @@ namespace TypeGen.Generators
         {
             foreach (var prop in source.Members.OfType<PropertyMember>())
             {
-                var clsMember = new PropertyMember(prop.Name);                
-                var koParam = MapReference(prop.MemberType, out var koType);
-                clsMember.Initialization = new RawStatements(koType, "<", koParam, ">()");
-                targetClass.Members.Add(clsMember);
+                GenerateMember(targetClass, prop);
             }
+        }
 
+        protected virtual void GenerateMember(ClassType targetClass, PropertyMember prop)
+        {
+            var clsMember = CreateMember(prop);
+            if (clsMember != null)
+                targetClass.Members.Add(clsMember);
+        }
+
+        protected virtual PropertyMember CreateMember(PropertyMember prop)
+        {
+            var clsMember = new PropertyMember(prop.Name);
+            var koParam = MapReference(prop.MemberType, out var koType);
+            clsMember.Initialization = new RawStatements(koType, "<", koParam, ">()");
+            //clsMember.Comment =  ReflectionGeneratorBase.GetGeneratedType(prop.MemberType) + "";
+            clsMember.ExtraData.Merge(prop.ExtraData);
+            clsMember.ExtraData[KO_ITEMTYPE] = koParam;
+            clsMember.ExtraData[KO_TYPE] = koType;
+
+            return clsMember;
         }
 
         protected virtual TypescriptTypeReference MapReference(TypescriptTypeReference typeRef, out string koType)
@@ -81,28 +105,35 @@ namespace TypeGen.Generators
             koType = "ko.observable";
             if (typeRef.ReferencedType is ArrayType arr)
             {
-                koType = "ko.observableArray";
-                typeRef = arr.ElementType;
+                koType = "ko.observableArray";                
+                return MapReference(arr.ElementType, out _);
             }
             if (typeRef.ReferencedType is InterfaceType intf)
             {
-                if (_map.TryGetValue(intf, out var mapped))
-                {
-                    typeRef = mapped;
-                }
-                else
-                {
-                    //generate?
-                    typeRef = GetClass(intf);
-                }
+                return Remap(intf, typeRef);
             }
             return typeRef;
         }
 
-        public ClassType GetClass(InterfaceType intf)
+        private TypescriptTypeReference Remap(InterfaceType sourceType, TypescriptTypeReference typeRef)
         {
-            if (_map.TryGetValue(intf, out var cls))
-                return cls;
+            if (!_map.TryGetValue(sourceType, out var mapped))
+            {
+                //generate?
+                mapped = GetInterfaceMap(sourceType);
+            }
+            var result = new TypescriptTypeReference(mapped);
+            if (mapped.IsGeneric)
+            {
+                result.GenericParameters.AddRange(typeRef.GenericParameters);
+            }
+            return result;
+        }
+
+        public DeclarationBase GetInterfaceMap(InterfaceType intf)
+        {
+            if (_map.TryGetValue(intf, out var result))
+                return result;
             return AddToQueue(intf);
         }
 
@@ -113,6 +144,11 @@ namespace TypeGen.Generators
             _targetModule.Members.Add(cls);
             _toGenerate.Add(intf);
             return cls;
+        }
+
+        public void AddMap(InterfaceType intf, DeclarationBase target)
+        {
+            _map[intf] = target;            
         }
 
         protected virtual string GetClassName(InterfaceType intf)
